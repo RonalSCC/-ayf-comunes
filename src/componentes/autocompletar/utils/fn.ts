41a479a36c0c1fb9f71eada62cb57937c72eb617
Ronal Santiago Castaño Chaparro <ronal.castano@sinco.com.co>
2023-04-12 09:34:24 -0500

    Primer componente

---
+++ src/componentes/autocompletar/utils/fn.ts
@@ -0,0 +1,31 @@
+export function isObject(obj: any) {
+  if ( !obj || toString.call( obj ) !== "[object Object]" )
+    return false;
+
+  return true;
+}
+
+export function isNotOject(obj: any) {
+  return !isObject(obj)
+}
+
+export function isEmptyObject(obj: any) {
+  for (const key in obj) {
+    return false;
+  }
+
+  return true
+};
+
+export function isNotEmptyObject(obj: any) {
+  return !isEmptyObject(obj)
+}
+
+export function defaultFunction(...args: any) { };
+
+export function normalizeString(str: string | number) {
+  return str
+    .toString()
+    .trim()
+    .toLocaleLowerCase();
+}
---
+++ src/componentes/index.ts
@@ -0,0 +1,1 @@
+export {default as AutocompletarAYF}  from './autocompletar'
